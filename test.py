5c83d68777a3557198b8c176bce75ff3a85fdd05
obi150 <161379869+obi150@users.noreply.github.com>
2024-02-29 10:23:53 -0800

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,12 @@ import cv2
 import urllib.request
 import numpy as np
 
-while True:
-    req = urllib.request.urlopen('http://192.168.43.47/capture')
-    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-    img = cv2.imdecode(arr, -1)  # 'Load it as it is'
+cap = cv2.VideoCapture('http://192.168.100.55:81/stream')
 
-    cv2.imshow('random_title', img)
-    
-    # Refresh the image after 100 milliseconds
-    if cv2.waitKey(100) & 0xff == 27:
-        break  # Exit the loop if the 'Esc' key is pressed
+while True:
+    if cap.isOpened():
+        ret, frame = cap.read()
+        cv2.imshow("ESP32-cam_Capture", frame)
+        cv2.waitKey(1)
 
 cv2.destroyAllWindows()
